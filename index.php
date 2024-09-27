13ecb652da4016b7c4f384e3dc210794002d0362
Raisul Islam <mdraju3248@gmail.com>
2024-09-27 12:47:18 +0600

    Added first name and last name label input

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Crud Application</title>
+
+    <!-- tailwind css  -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            PHP Crud Application
+        </h1>
+        <div>
+            <form action="" method="post">
+                <div>
+                    <label for="name">First Name</label>
+                    <input type="text" name="fname" id="name" class="border border-gray-400 p-2">
+                </div>
+                <div>
+                    <label for="name">Last Name</label>
+                    <input type="text" name="lname" id="name" class="border border-gray-400 p-2">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
