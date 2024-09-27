44c051a35b03bc32d52236b0b8ef82eabad5913d
Raisul Islam <mdraju3248@gmail.com>
2024-09-28 00:24:08 +0600

    Added crud data show in view file table create

---
+++ view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD Application</title>
+
+    <!-- tailwind css cdn-->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="max-w-[750px] mx-auto bg-white shadow-lg rounded-lg p-6 mt-10">
+        <table class="table-auto w-full">
+            <thead class="border-b-2 border-gray-200">
+                <tr class="text-left">
+                    <th class="px-4 py-2">First Name</th>
+                    <th class="px-4 py-2">Last Name</th>
+                    <th class="px-4 py-2">Age</th>
+                </tr>
+            </thead>
+
+        </table>
+    </div>
+</body>
+</html>
